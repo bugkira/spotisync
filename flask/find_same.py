f3ba30eea646bfed8a2dd1e5ddb07f993dbc0173
bugkira <128284937+bugkira@users.noreply.github.com>
2023-10-10 01:01:44 +0300

    Update find_same.py
    Убрал if __name__

--- flask/find_same.py
+++ flask/find_same.py
@@ -19,16 +19,3 @@ def artists(*users_ids: str) -> Set[str]:
         user_artists = User(user_id).artists()
         same_artists &= user_artists
     return same_artists
-
-
-if __name__ == "__main__":
-    user1 = User("kirabagaev")
-    user2 = User("newbugsbunny")
-    # tracks = get_user_tracks(user2_id)
-    tracks = tracks(user1, user2)
-    print("Общие треки:")
-    for track in tracks:
-        print(track)
-    # artists = artists(user1_id, user2_id)
-    # print("Общие артисты:")
-    # print(*artists, sep=", ")
